ea49d0a7b2f56a53765e828f24422c1da2923eec
Moshe Tanzer <tanzermt@gmail.com>
2025-05-06 06:53:26 +0200

    feat: types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,3 +3,10 @@ export interface damerauLevenshteinDistance {
     relative: number;
     similarity: number;
   }
+
+export type Vector = number[] | Float32Array | Float64Array;
+
+export interface StringVectorizationOptions {
+  tokenizeBy?: 'char' | 'word';
+  caseSensitive?: boolean;
+}
