87f28e764e47f24b7fc997565098d4124edab3b7
Moshe Tanzer <tanzermt@gmail.com>
2025-05-19 20:30:03 +0200

    feat: start adding presets only name up

--- src/presets/index.ts
+++ src/presets/index.ts
@@ -1,11 +1,28 @@
 import { titleCase } from '../normalizers/string-case'
 import { normalizePunctuationSpacing } from '../normalizers/whitespace'
 
-function formatName(text: string): string {
-  text = text.toLowerCase()
+function normalizeName(text: string): string {
+  text = text.toLowerCase().trim()
   text = normalizePunctuationSpacing(text)
   text = titleCase(text)
   return text
 }
+function normalizeEmail(text: string): string {
+  text = text.toLowerCase().trim()
+  return text
+}
+function normalizeDate(text: string): string {
+  text = text.trim()
+  return text
+}
+function normalizeUrl(text: string): string {
+  text = text.trim()
+  return text
+}
 
-export { formatName }
+export {
+  normalizeDate,
+  normalizeEmail,
+  normalizeName,
+  normalizeUrl,
+}
