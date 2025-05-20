59cef72a24b6ad89d66d0aeca15bfb4db485695a
Moshe Tanzer <tanzermt@gmail.com>
2025-05-20 08:49:14 +0200

    feat: add normalizeName preset

--- src/presets/presets.ts
+++ src/presets/presets.ts
@@ -1,28 +1,16 @@
+import { removeCombiningMarks, removeControlCharacters, removeDiacritics, removeIllegalCharacters, removeNonASCII } from '../normalizers/special-characters'
 import { titleCase } from '../normalizers/string-case'
-import { normalizePunctuationSpacing } from '../normalizers/whitespace'
+import { normalizePunctuationSpacing, normalizeWhitespace, removeExtraSpaces } from '../normalizers/whitespace'
 
 function normalizeName(text: string): string {
   text = text.toLowerCase().trim()
   text = normalizePunctuationSpacing(text)
+  text = removeCombiningMarks(removeControlCharacters(removeDiacritics(removeIllegalCharacters(removeNonASCII(text)))))
+  text = normalizeWhitespace(removeExtraSpaces(normalizePunctuationSpacing((text))))
   text = titleCase(text)
-  return text
-}
-function normalizeEmail(text: string): string {
-  text = text.toLowerCase().trim()
-  return text
-}
-function normalizeDate(text: string): string {
-  text = text.trim()
-  return text
-}
-function normalizeUrl(text: string): string {
-  text = text.trim()
   return text
 }
 
 export {
-  normalizeDate,
-  normalizeEmail,
-  normalizeName,
-  normalizeUrl,
+  normalizeName
 }
